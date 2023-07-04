93a6c72ac8e20539c9e85e965f9fd2b204dcb91e
Giuseppe Di Mauro <azrafe7@gmail.com>
2023-07-04 18:58:30 +0200

    if image is not CORS protected, send as url...
    else fetch as dataUrl and use that

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+let manifest = chrome.runtime.getManifest();
+console.log(manifest.name + " v" + manifest.version);
+
+const photopeaUrl = "https://www.photopea.com";
+
 chrome.runtime.onInstalled.addListener(() => {
 	chrome.contextMenus.create({
 	  id: "onImageContextMenu",
@@ -14,68 +19,77 @@ function blobToDataUrl(blob) {
   ).then(e => e.target.result);
 }
 
+async function sendAsDataURL(info, tab) {
+  let img = await fetch(info.srcUrl);
+  let blob = await img.blob();
+  let dataURL = await blobToDataUrl(blob);
+  console.log(dataURL);
+
+  chrome.tabs.create({url: photopeaUrl}, (tab) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      args: [dataURL],
+      func: (dataURL) => {
+        let photopeaInited = false;
+        
+        window.addEventListener("message", (e) => {
+          // alert(e.data);
+          // console.log(e);
+          if (e.data?.type === "HelloMessage") {
+            console.log("[Send2Photopea:PP] INITED");
+            photopeaInited = true;
+          };
+        });
+        
+        let message = `app.open("${dataURL}")`;
+        // console.log("[Send2Photopea:PP] tryPostMessage\n" + message);
+        
+        function tryPostMessage() {
+          if (photopeaInited) {
+            console.log("[Send2Photopea:PP] postMessage\n" + message);
+            window.postMessage(message, "*");
+          } else {
+            setTimeout(tryPostMessage, 100);
+          }
+        }
+        tryPostMessage();
+      },
+    })
+    .then(() => {
+      console.log("[Send2Photopea:BG] script injected");
+    });
+  });
+}
+
+
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   console.log("[Send2Photopea:BG] onContextMenuClicked:", [info, tab]);
 
   if (info.menuItemId === "onImageContextMenu") {
     if (info.mediaType === "image") {
-      let img = await fetch(info.srcUrl);
-      let blob = await img.blob();
-      let dataURL = await blobToDataUrl(blob);
-      console.log(dataURL);
-
-      let config = {"files":[dataURL], "environment":{}};
-      let encodedConfig = encodeURI(JSON.stringify(config));
-
-      const photopeaUrl = "https://www.photopea.com";
-
-      chrome.tabs.create({url: photopeaUrl}, (tab) => {
-        chrome.scripting.executeScript({
-          target: {tabId: tab.id},
-          args: [dataURL],
-          func: (dataURL) => {
-            let photopeaInited = false;
             
-            window.addEventListener("message", (e) => {
-              // alert(e.data);
-              // console.log(e);
-              if (e.data?.type === "HelloMessage") {
-                console.log("[Send2Photopea:PP] INITED");
-                photopeaInited = true;
-              };
+      chrome.tabs.sendMessage(
+        tab.id,
+        {
+          event: "sendToPhotopea",
+          data: info,
+        }
+        
+        ,
+        function(response) {
+          console.log("[Send2Photopea:BG] send as " + response?.sendAs);
+          if (response.sendAs === "dataURL") {
+            sendAsDataURL(info, tab);
+          } else {
+            let config = {"files":[info.srcUrl], "environment":{}};
+            let encodedConfig = encodeURI(JSON.stringify(config));
+
+            chrome.tabs.create({
+              url: photopeaUrl + "#" + encodedConfig,
             });
-            
-            let message = `app.open("${dataURL}")`;
-            // console.log("[Send2Photopea:PP] tryPostMessage\n" + message);
-            
-            function tryPostMessage() {
-              if (photopeaInited) {
-                console.log("[Send2Photopea:PP] postMessage\n" + message);
-                window.postMessage(message, "*");
-              } else {
-                setTimeout(tryPostMessage, 100);
-              }
-            }
-            tryPostMessage();
-          },
-        })
-        .then(() => {
-          console.log("[Send2Photopea:BG] script injected");
-        });
-      });
-
-
-      // chrome.tabs.create({
-      //  url: photopeaUrl + "#" + encodedConfig,
-      // });
-      
-      // chrome.tabs.sendMessage(
-      //   tab.id,
-      //   {
-      //     event: "sendToPhotopea",
-      //     data: photopeaUrl + "#" + encodedConfig,
-      //   }
-      // );
+          }
+        }
+      );
     }
   }
 });
--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,34 @@
 (() => {
-  chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
+  let manifest = chrome.runtime.getManifest();
+  console.log(manifest.name + " v" + manifest.version);
+
+  async function fetchData(url) {
+    let response = {};
+
+    try {
+      let img = await fetch(url);
+      console.log("[Send2Photopea:CTX] can fetch image " + url);
+      response = {sendAs: "url"};
+    } catch (e) {
+      console.log("[Send2Photopea:CTX] cannot fetch image " + url);
+      console.log(e);
+      response = {sendAs: "dataURL"};
+    }
+    
+    return response;
+  }
+
+  chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     console.log("[Send2Photopea:CTX]", msg);
     const { event, data } = msg;
 
-	const photopeaUrl = "https://www.photopea.com";
+    const photopeaUrl = "https://www.photopea.com";
 	
-	if (event === "sendToPhotopea") {
-      window.open(photopeaUrl + "#" + data, '_blank');
+    
+    if (event === "sendToPhotopea") {
+      fetchData(data.srcUrl).then(sendResponse);
     }
+    
+    return true; // keep port alive
   });
 })();
