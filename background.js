c81785bb0d81b2b15d8ec4ab66ba8803edd2b839
Giuseppe Di Mauro <azrafe7@gmail.com>
2023-07-06 23:08:07 +0200

    refactor: postMessage, wait, focus, etc.

--- background.js
+++ background.js
@@ -19,46 +19,100 @@ function blobToDataUrl(blob) {
   ).then(e => e.target.result);
 }
 
+async function waitForPhotopeaInitAndPostMessage(tab, message) {
+  return new Promise((resolve, reject) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      args: [message],
+      func: (message) => {
+        let photopeaInited = false;
+
+        window.addEventListener("message", (e) => {
+          // alert("[Send2Photopea:PP]\n" + e.data);
+          console.log("[Send2Photopea:PP]", e);
+          if (e.data?.type === "HelloMessage") {
+            console.log("[Send2Photopea:PP] INITED");
+            photopeaInited = true;
+          }
+        });
+
+        function tryPostMessage() {
+          if (photopeaInited) {
+            console.log("[Send2Photopea:PP] wait and postMessage\n" + message);
+            window.postMessage(message, "*");
+          } else {
+            setTimeout(tryPostMessage, 100);
+          }
+        }
+
+        tryPostMessage();
+      },
+    })
+    .then(() => {
+      console.log("[Send2Photopea:BG.waitForPhotopeaInitAndPostMessage] script injected");
+    });
+  });
+}
+
+async function focusTab(tab) {
+  return new Promise((resolve, reject) => {
+    var updateProperties = { 'active': true };
+    chrome.tabs.update(tab.id, updateProperties, (tab) => { resolve(tab); });
+  });
+}
+
+async function getPhotopeaTab() {
+  let queryOptions = { url: "https://www.photopea.com/" };
+  let isInited = true;
+
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [photopeaTab] = await chrome.tabs.query(queryOptions);
+  if (photopeaTab === undefined) {
+    console.log(`[Send2Photopea:BG] opening new Photopea tab...`);
+    return new Promise((resolve, reject) => {
+      isInited = false;
+      chrome.tabs.create({url: photopeaUrl}, (tab) => {
+        console.log(`[Send2Photopea:BG] opened new Photopea tab (idx: ${tab.index})`);
+        resolve({photopeaTab: tab, isInited: isInited});
+      });
+    });
+  }
+
+  console.log(`[Send2Photopea:BG] found open Photopea tab (idx: ${photopeaTab.index})`);
+  return {photopeaTab: photopeaTab, isInited: isInited};
+}
+
+function postMessage(photopeaTab, message) {
+  chrome.scripting.executeScript({
+    target: {tabId: photopeaTab.id},
+    args: [message],
+    func: (message) => {
+      console.log("[Send2Photopea:PP] postMessage\n" + message);
+      window.postMessage(message, "*");
+    },
+  })
+  .then(() => {
+    console.log("[Send2Photopea:BG.postMessage] script injected");
+  });
+}
+
 async function sendAsDataURL(info, tab) {
   let img = await fetch(info.srcUrl);
   let blob = await img.blob();
   let dataURL = await blobToDataUrl(blob);
   console.log(dataURL);
 
-  chrome.tabs.create({url: photopeaUrl}, (tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      args: [dataURL],
-      func: (dataURL) => {
-        let photopeaInited = false;
+  let {photopeaTab, isInited} = await getPhotopeaTab();
+  // console.log(photopeaTab, isInited);
+  await focusTab(photopeaTab);
 
-        window.addEventListener("message", (e) => {
-          // alert(e.data);
-          // console.log(e);
-          if (e.data?.type === "HelloMessage") {
-            console.log("[Send2Photopea:PP] INITED");
-            photopeaInited = true;
-          };
-        });
+  let message = `app.open("${dataURL}")`;
 
-        let message = `app.open("${dataURL}")`;
-        // console.log("[Send2Photopea:PP] tryPostMessage\n" + message);
-
-        function tryPostMessage() {
-          if (photopeaInited) {
-            console.log("[Send2Photopea:PP] postMessage\n" + message);
-            window.postMessage(message, "*");
-          } else {
-            setTimeout(tryPostMessage, 100);
-          }
-        }
-        tryPostMessage();
-      },
-    })
-    .then(() => {
-      console.log("[Send2Photopea:BG] script injected");
-    });
-  });
+  if (!isInited) {
+    waitForPhotopeaInitAndPostMessage(photopeaTab, message);
+  } else {
+    postMessage(photopeaTab, message);
+  }
 }
 
 function sendAsFile(info, tab) {
@@ -75,7 +129,7 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
 
   if (info.menuItemId === "onImageContextMenu") {
     if (info.mediaType === "image") {
-      
+
       // on the webstore page no content script is injected
       // so we sendAsFile directly
       if (tab.url.startsWith("https://chrome.google.com/webstore")) {
